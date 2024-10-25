7c37f5683c4814929472229cf0189b6151504d07
SolidElectronics <git@solidelectronics.net>
2024-10-24 22:28:12 -0400

    Fix spacing

--- keyboards/handwired/3by3/keymaps/vial/keymap.c
+++ keyboards/handwired/3by3/keymaps/vial/keymap.c
@@ -1,21 +1,9 @@
- /* This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 2 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
+// SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
 enum layers{
-  _MAIN,
-  _ALT
+	_MAIN,
+	_ALT
 };
 
 enum jiggle_keycode {
@@ -116,5 +104,5 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 			}
 		}
 	}
-    return false;
+	return false;
 }
